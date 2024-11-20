b5375f78f7c6912b4069d465110992979c42ec3f
Elmoataz Abdellah <147349819+mezosauce@users.noreply.github.com>
2024-11-19 21:46:12 -0500

    Update Medicine.java

--- src/main/java/edu/uc/cs3003/medava/Medicine.java
+++ src/main/java/edu/uc/cs3003/medava/Medicine.java
@@ -2,9 +2,34 @@ package edu.uc.cs3003.medava;
 
 public class Medicine {
 
+    //Private val that is updated from constructor indirectly from user
+    private String mMedicineName;
+    
+    //Constructor
     public Medicine(String medicineName){
         mMedicineName = medicineName;
     }
-    private String mMedicineName;
+    
+    //Getter 
+    public String getMedicineName() {
+        return mMedicineName;
+
+      }
+      //Logic functionality of Pharmacy simulation
+    public boolean isTemperatureRangeAcceptable(Double lowTemperature, Double highTemperature) {
+        if (this.minimumTemperature() <= lowTemperature &&
+                highTemperature <= this.maximumTemperature()) {
+            return true;
+        }
+        return false;
+    }
+    //Methods used by function ^^^^^ for the this.function operation
+    public double minimumTemperature() {
+        return 0.0;
+    }
+
+    public double maximumTemperature() {
+        return 100.0;
+    }
 
 }
