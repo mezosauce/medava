53ceafc7463327cc51693fb372fccfb8ae35e555
Elmoataz Abdellah <147349819+mezosauce@users.noreply.github.com>
2024-11-19 21:46:15 -0500

    Update Transporter.java

--- src/main/java/edu/uc/cs3003/medava/Transporter.java
+++ src/main/java/edu/uc/cs3003/medava/Transporter.java
@@ -1,3 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
 package edu.uc.cs3003.medava;
+
+import java.util.List;
+
 public class Transporter {
+    //Private value updated indirectly by user
+    private String mTransporterName;
+    //Constructor
+    public String getTransporterName() {
+        return mTransporterName;
+    }
+    // Private list of "Medicine class" denoted as goods
+    Public List<Medicine> goods;
+    {
+        goods = new ArrayList<Medicine>();
+    }
 }
