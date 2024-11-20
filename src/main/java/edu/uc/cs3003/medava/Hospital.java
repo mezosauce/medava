142c86adb981ff3fefbbfd9e18e87ddc56c0dbf1
Elmoataz Abdellah <147349819+mezosauce@users.noreply.github.com>
2024-11-19 21:46:11 -0500

    Update Hospital.java

--- src/main/java/edu/uc/cs3003/medava/Hospital.java
+++ src/main/java/edu/uc/cs3003/medava/Hospital.java
@@ -1,1 +1,21 @@
 package edu.uc.cs3003.medava;
+
+public class Hospital {
+
+    public Hospital(String hospitalName) {
+        name = hospitalName;
+    }
+
+    void receive(Transporter t) {
+        while (!t.goods.isEmpty()) {
+            Medicine unloaded = t.goods.remove(0);
+            System.out.println(String.format("Receiving an %s.", unloaded.getMedicineName()));
+        }
+    }
+
+    public String name() {
+        return name;
+    }
+
+    private String name;
+}
