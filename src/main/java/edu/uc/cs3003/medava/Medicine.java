0bb4b4c32800117242fa8527f207eb7d3e747704
Elmoataz Abdellah <147349819+mezosauce@users.noreply.github.com>
2024-11-19 20:55:57 -0500

    Update Medicine.java

--- src/main/java/edu/uc/cs3003/medava/Medicine.java
+++ src/main/java/edu/uc/cs3003/medava/Medicine.java
@@ -2,4 +2,9 @@ package edu.uc.cs3003.medava;
 
 public class Medicine {
 
+    public Medicine(String medicineName){
+        mMedicineName = medicineName;
+    }
+    private String mMedicineName;
+
 }
